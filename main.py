8a9456dd0ab9b97b92442cc2deedda8d817fb6a0
Towel Bunny <vyongxuan1@gmail.com>
2026-01-30 15:44:45 +0800

    update main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import cv2
-import numpy as np
-import RPi.GPIO as GPIO
-from picamera2 import Picamera2
+#-- Libraries --
+import cv2                          # image processing
+import numpy as np                  # process pixels
+import RPi.GPIO as GPIO             # control GPIO pins
+from picamera2 import Picamera2     # camera
 
 # --- MOTOR SETUP ---
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)              
 
 # Left Motor pins
 ENA, IN1, IN2 = 12, 23, 24
@@ -13,14 +14,17 @@ ENA, IN1, IN2 = 12, 23, 24
 ENB, IN3, IN4 = 13, 17, 27
 
 GPIO.setup([IN1, IN2, ENA, IN3, IN4, ENB], GPIO.OUT)
-pwmA = GPIO.PWM(ENA, 1000); pwmB = GPIO.PWM(ENB, 1000)
-pwmA.start(0); pwmB.start(0)
+pwmA = GPIO.PWM(ENA, 1000)
+pwmB = GPIO.PWM(ENB, 1000)
+
+pwmA.start(0)
+pwmB.start(0)
 
 last_error = 0
 Kp = 0.7
 
 def move_robot(error):
-    global last_error
+    global last_error # Use global to remember last error
     
     # 1. Calculate how fast the error is changing (Derivative)
     # If this is a huge number, we are hitting a sharp turn
@@ -46,14 +50,16 @@ def move_robot(error):
         # 3. Normal Steering - SLOWER base speed for better accuracy
         base_speed = 50 
         kp = 1.2
-        steering = error * kp 
+        steering = error * kp  #higher error -> more steering adjustment
 
-        GPIO.output(IN1, GPIO.LOW); GPIO.output(IN2, GPIO.HIGH)
-        GPIO.output(IN3, GPIO.LOW); GPIO.output(IN4, GPIO.HIGH)
+        #move forward
+        GPIO.output(IN1, GPIO.LOW); GPIO.output(IN2, GPIO.HIGH) 
+        GPIO.output(IN3, GPIO.LOW); GPIO.output(IN4, GPIO.HIGH) 
         
         l_speed = base_speed + steering
         r_speed = base_speed - steering
 
+        #allow speed between 20 and 100
         pwmA.ChangeDutyCycle(max(20, min(100, l_speed)))
         pwmB.ChangeDutyCycle(max(20, min(100, r_speed)))
         
@@ -83,25 +89,27 @@ def stop_motors():
 
 # --- CAMERA SETUP ---
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (160, 120)})
+config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (160, 120)}) #160 pixels wide, 120 pixels tall
 picam2.configure(config)
 picam2.start()
 
 try:
     while True:
-        frame = picam2.capture_array()
-        color_view = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        gray = cv2.cvtColor(color_view, cv2.COLOR_BGR2GRAY)
-        roi = gray[20:80, 0:160]
-        blur = cv2.GaussianBlur(roi, (5, 5), 0)
-        _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        frame = picam2.capture_array()                       #store image into grid of numbers
+        color_view = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) #convert RGB to BGR for OpenCV
+        gray = cv2.cvtColor(color_view, cv2.COLOR_BGR2GRAY) #convert BGR to Grayscale
+        roi = gray[20:80, 0:160]                            #region of interest (crop to area around line) 20 to 80 vertical, 0 to 160 horizontal
+        blur = cv2.GaussianBlur(roi, (5, 5), 0)             #blur out 
+        _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU) #threshold to binary image, invert black and white
 
         M = cv2.moments(thresh)
         if M['m00'] > 500:
             cx = int(M['m10'] / M['m00'])
             # HERE IS WHERE WE GET THE ERROR
-            error = cx - 80 
-            
+            error = cx - 80                     #balance point is 80, so error is how far from center the line is
+                                                #pos -> robot too left -> turn right
+                                                #neg -> robot too right -> turn left
+
             # If the line is far to the side, save that memory!
             if abs(error) > 40:
                 last_error = error
